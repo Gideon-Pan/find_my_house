13922c13fac7a511cdad8ee8f98b21ace9225a96
Gideon Pan <s00971052s@gmail.com>
2021-10-26 10:24:13 +0800

    fetch bus data and insert into monogodb

---
+++ model/db/mongodb/mongo_helper.js
@@ -0,0 +1,50 @@
+const { MongoClient } = require('mongodb')
+// const getPtxData = require('../../../ptx_testing/metro-ptx');
+const { MONGO_PASSWORD, MONGO_DATABASE } = process.env
+
+const uri = MONGO_PASSWORD
+  ? `mongodb+srv://Gideon:${MONGO_PASSWORD}@cluster0.0fwjx.mongodb.net/${MONGO_DATABASE}?retryWrites=true&w=majority`
+  : 'mongodb://127.0.0.1:27017'
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+async function createCollection(collectionName) {
+  await client.connect()
+  const db = client.db('ptx')
+  await db.createCollection(collectionName)
+  console.log('Collection created')
+  await client.close()
+}
+
+async function insertMany(collection, data) {
+  await client.connect()
+  const db = client.db('ptx')
+  await db.collection(collection).insertMany(data)
+  console.log(`finishing insert data into ${collection} Collection`)
+  await client.close()
+}
+
+async function getMongoData(collection) {
+  await client.connect()
+  const db = client.db('ptx')
+  const data = await db.collection(collection).find({}).toArray()
+  await client.close()
+  return data
+}
+
+// async function getCollectionSize(collectionName) {
+//   await client.connect()
+//   const db = client.db("ptx")
+//   const size = await db.collection(collectionName).total
+//   // console.log('Collection created')
+//   console.log(size)
+//   await client.close()
+// }
+
+module.exports = {
+  createCollection,
+  insertMany,
+  getMongoData
+}
---
+++ ptx/bus/insert_mongo.js
@@ -0,0 +1,117 @@
+const getPtxData = require('../metro-ptx')
+const { insertMany } = require('../model/mongo/mongo-helper')
+
+async function insertBusStations() {
+  const busStationsTPE = await getPtxData(
+    'https://ptx.transportdata.tw/MOTC/v2/Bus/Station/City/Taipei?$top=10000&$format=JSON'
+  )
+  // await insertMany('busStations', busStationsTPE)
+  console.log(busStationsTPE.length)
+  const busStationsNTP = await getPtxData(
+    'https://ptx.transportdata.tw/MOTC/v2/Bus/Station/City/NewTaipei?$top=10000&$format=JSON'
+  )
+  console.log(busStationsNTP.length)
+  const busStationsAll = busStationsTPE.concat(busStationsNTP)
+  console.log(busStationsAll.length)
+  await insertMany('busStations', busStationsAll)
+  console.log('nice')
+}
+
+async function insertBusStops() {
+  const busStopsTPE = await getPtxData(
+    'https://ptx.transportdata.tw/MOTC/v2/Bus/Stop/City/Taipei?$top=100000&$format=JSON'
+  )
+  // await insertMany('busStops', busStopsTPE)
+  console.log(busStopsTPE.length)
+  const busStopsNTP = await getPtxData(
+    'https://ptx.transportdata.tw/MOTC/v2/Bus/Stop/City/NewTaipei?$top=100000&$format=JSON'
+  )
+  console.log(busStopsNTP.length)
+  const busStopsAll = busStopsTPE.concat(busStopsNTP)
+  console.log(busStopsAll.length)
+  await insertMany('busStops', busStopsAll)
+  console.log('nice')
+}
+
+async function insertbusRoutes() {
+  const busRoutesTPE = await getPtxData(
+    'https://ptx.transportdata.tw/MOTC/v2/Bus/DisplayStopOfRoute/City/Taipei?$top=10000&$format=JSON'
+  )
+  console.log(busRoutesTPE.length)
+  const busRoutesNTP = await getPtxData(
+    'https://ptx.transportdata.tw/MOTC/v2/Bus/DisplayStopOfRoute/City/NewTaipei?$top=10000&$format=JSON'
+  )
+  console.log(busRoutesNTP.length)
+  const busRoutesAll = busRoutesTPE.concat(busRoutesNTP)
+  console.log(busRoutesAll.length)
+  // busRoutesAll.forEach(route => {
+  //   route.Stops.forEach(stop => {
+  //     if (stop.StopName.Zh_tw === '丹鳳國小') {
+  //       console.log(route.RouteName)
+  //     }
+  //   })
+  // })
+  await insertMany('busRoutes', busRoutesAll)
+  console.log('done')
+}
+
+async function insertBusWaitingTime() {
+  const busWaitingTimeTPE = await getPtxData(
+    'https://ptx.transportdata.tw/MOTC/v2/Bus/EstimatedTimeOfArrival/City/Taipei?$top=100000&$format=JSON'
+  )
+  console.log(busWaitingTimeTPE.length)
+  const busWaitingTimeNTP = await getPtxData(
+    'https://ptx.transportdata.tw/MOTC/v2/Bus/EstimatedTimeOfArrival/City/NewTaipei?$top=100000&$format=JSON'
+  )
+  console.log(busWaitingTimeNTP.length)
+  // const busStopsNTP = await getPtxData("https://ptx.transportdata.tw/MOTC/v2/Bus/Stop/City/NewTaipei?$top=100000&$format=JSON")
+  // console.log(busStopsNTP.length)
+  // const busStopsAll = busStopsTPE.concat(busStopsNTP)
+  // console.log(busStopsAll.length)
+  const busWaitingTimeAll = busWaitingTimeTPE.concat(busWaitingTimeNTP)
+  console.log(busWaitingTimeAll.length)
+  await insertMany('busWaitingTime', busWaitingTimeAll)
+  console.log('done')
+}
+
+async function insertBusWaitingTimeTiny() {
+  const busWaitingTimeTPE = await getPtxData(
+    'https://ptx.transportdata.tw/MOTC/v2/Bus/EstimatedTimeOfArrival/City/Taipei?$top=100000&$format=JSON'
+  )
+  console.log(busWaitingTimeTPE.length)
+  const busWaitingTimeNTP = await getPtxData(
+    'https://ptx.transportdata.tw/MOTC/v2/Bus/EstimatedTimeOfArrival/City/NewTaipei?$top=100000&$format=JSON'
+  )
+  console.log(busWaitingTimeNTP.length)
+  // const busStopsNTP = await getPtxData("https://ptx.transportdata.tw/MOTC/v2/Bus/Stop/City/NewTaipei?$top=100000&$format=JSON")
+  // console.log(busStopsNTP.length)
+  // const busStopsAll = busStopsTPE.concat(busStopsNTP)
+  // console.log(busStopsAll.length)
+  const busWaitingTimeAll = busWaitingTimeTPE.concat(busWaitingTimeNTP)
+  const data = busWaitingTimeAll.map(
+    ({ StopID, Direction, UpdateTime, EstimateTime, StopStatus }) => ({
+      StopID,
+      Direction,
+      EstimateTime,
+      StopStatus,
+      UpdateTime
+    })
+  )
+  console.log(data.length)
+  await insertMany('busWaitingTime', data)
+  console.log('finsish insert bus waiting time data')
+}
+
+async function insertBusAvgWaitingTime(busAvgWaitingTime) {
+  await insertMany('busAvgWaitingTime', busAvgWaitingTime)
+  console.log('done')
+}
+
+module.exports = {
+  insertBusStations,
+  insertBusStops,
+  insertBusWaitingTime,
+  insertBusWaitingTimeTiny,
+  insertbusRoutes,
+  insertBusAvgWaitingTime
+}
